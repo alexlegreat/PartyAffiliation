e179d4f236c4b14410c5395b6e3ba7507ac08ed9
alexlegreat <yabrofaj@mail.uc.edu>
2024-09-26 15:08:22 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+
+    {
+        Scanner in = new Scanner(System.in);
+        String partyAfil = "";
+
+        System.out.println("Enter your party affiliation [R,D,I]: ");
+        partyAfil = in.nextLine();
+
+        if(partyAfil.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant ");
+        }
+        else if(partyAfil.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if(partyAfil.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get a Independent Man!");
+        }
+        else
+            System.out.println("I don't know what party this is: " + partyAfil);
+
+
+
+
+
+
+
+
+
+
     }
 }
